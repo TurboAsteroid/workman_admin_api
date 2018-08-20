c68ad83ff2be05207918a69b8d17b705981049bd
Vasily Lobarev <vlobarev@gmail.com>
2018-08-20 12:34:34 +0500

    вроде норм

--- app.js
+++ app.js
@@ -47,22 +47,4 @@ require('./routes/users')(app, config, firebase_admin, router);
 require('./routes/tags')(app, config, router);
 
 app.use(router);
-
-// app.use(express.static(path.join(__dirname, 'public')));
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 module.exports = app;
