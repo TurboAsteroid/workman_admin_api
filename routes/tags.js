ae6cc99046b8faae8ecb5654a6decaf5972c1ea6
Vasily Lobarev <vlobarev@gmail.com>
2018-08-20 15:07:30 +0500

    small fix

--- routes/tags.js
+++ routes/tags.js
@@ -15,12 +15,5 @@ module.exports = function(app, config, router) {
         connection.close();
     });
 
-    router.get('/tags/get', async function (req, res, next) {
-        const connection = await mysql.createConnection(mysql_config);
-        const [rows1, fields1] = await connection.execute(`select tags.id as value, tags.text from tags`);
-        res.json(rows1);
-        connection.close();
-    });
-
     return router;
 };
