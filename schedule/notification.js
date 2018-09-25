6dac0cc792b06c9295db468aa373f9f697901db5
Vasily Lobarev <vlobarev@gmail.com>
2018-09-25 08:29:49 +0500

    table name port

--- schedule/notification.js
+++ schedule/notification.js
@@ -42,7 +42,7 @@ module.exports = function(app, config, firebase_admin) {
     async function getNotificationList() {
         const connection = await mysql.createConnection(mysql_config);
         // Новые уведомления / первой линии
-        const [rows1, fields1] = await connection.execute('select * from incidentGroups where complete = 0 AND (time_sent IS NULL)', []);
+        const [rows1, fields1] = await connection.execute('select * from incidentgroups where complete = 0 AND (time_sent IS NULL)', []);
         // console.log("rows1", rows1);
         for (let i in rows1) {
             let [rows, fields] = await connection.execute('select * from GroupRows left join GroupRowUsers on GroupRowUsers.row_id = GroupRows.id left join users on GroupRowUsers.user_id = users.id where GroupRows.group_id = ? and row_number = ?', [ rows1[i].group_id, rows1[i].current_row ]);
@@ -50,7 +50,7 @@ module.exports = function(app, config, firebase_admin) {
                 await createNotification(rows1[i].id, rows[j].row_id, rows[j].user_id, rows1[i].group_id, rows1[i].incident_id);
             }
             // console.log("rows1[i].incidentGroup_id, rows1[i].group_id, rows1[i].current_row", rows1[i].id, rows1[i].group_id, rows1[i].current_row);
-            let [upd_rows, upd_fields] = await connection.execute('update incidentGroups  SET time_sent = NOW() where id = ? and group_id = ? and current_row = ?', [rows1[i].id, rows1[i].group_id, rows1[i].current_row ]);
+            let [upd_rows, upd_fields] = await connection.execute('update incidentgroups  SET time_sent = NOW() where id = ? and group_id = ? and current_row = ?', [rows1[i].id, rows1[i].group_id, rows1[i].current_row ]);
         }
 
         const [rows2, fields2] = await connection.execute('select incidentgroups.*, grouprows.row_number, grouprows.delay, group_max_row.max_row ' +
