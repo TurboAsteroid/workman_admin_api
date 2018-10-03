1c6c713db0c9db8d52c9f138723aafaa6cf3872a
vlobarev <v.lobarev@elem.ru>
2018-10-03 10:13:50 +0500

    docker, добавлены сертификаты

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var cors = require('cors');
 var app = express();
 var config = require('./config');
 var firebase_admin = require('firebase-admin');
-var serviceAccount = require('./alertnotification-a0fd6-firebase-adminsdk-qbv0s-fa2aec8441.json');
+var serviceAccount = require('./alertnotification-a0fd6-firebase-adminsdk-qbv0s-7e9fcb94aa.json');
 
 
 firebase_admin.initializeApp({
     credential: firebase_admin.credential.cert(serviceAccount),
-    databaseURL: 'https://alertnotification-a0fd6.firebaseio.com/'
+    databaseURL: 'https://alertnotification-a0fd6.firebaseio.com'
 });
 
 app.use(cors({origin: '*'}));
--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    'dbHost' : 'localhost',
+    'dbHost' : '10.1.255.117',
     'dbDatabase' : 'alertnotification',
     'dbUser': 'alnot',
     'dbPassword': 'df734##12iiRR2==-',
 
-    'ldapurl': 'ldap://elem-dc0.elem.ru',
+    'ldapurl': 'ldap://10.1.255.29',
     'ldapbaseDN': 'dc=elem,dc=ru',
     'username': 'gs2@elem.ru',
     'password': 'gs2-1',
